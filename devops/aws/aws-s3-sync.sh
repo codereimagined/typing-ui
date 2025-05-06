f11fd1db79762c56d9b2a91502bc150a8d2ee1cf
Sergey Petrunin <halfstrik@gmail.com>
2025-05-05 22:21:40 -0400

    Add cache invalidation upon deployment

--- devops/aws/aws-s3-sync.sh
+++ devops/aws/aws-s3-sync.sh
@@ -2,3 +2,7 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 aws s3 sync $SCRIPT_DIR/../../dist s3://codereimaginedtypingappaws-s3bucketroot-e9cabqeshsne --delete
+
+aws cloudfront create-invalidation \
+  --distribution-id E2VCVQ341CFRK1 \
+  --paths "/*"
