1b6d4de908766123b1f5d80356116dc70dec0575
Sergey Petrunin <halfstrik@gmail.com>
2025-05-07 22:32:55 -0400

    Improve aws-s3-sync.sh script to avoid confusion if we forget to build the project first

--- devops/aws/aws-s3-sync.sh
+++ devops/aws/aws-s3-sync.sh
@@ -1,5 +1,9 @@
+
 # Get the directory where the script is located
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+cd $SCRIPT_DIR/../..
+npm run build
 
 aws s3 sync $SCRIPT_DIR/../../dist s3://codereimaginedtypingappaws-s3bucketroot-e9cabqeshsne --delete
 
